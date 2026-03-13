b42d0528a1c4cbae4bf870c82af0f2e1d8e934bd
Jonny <Jonny@MacBook-Air-de-Louise.local>
2026-03-13 10:54:52 +0000

    Completed the excel file for contact emails

--- Fuzzymatching.py
+++ Fuzzymatching.py
@@ -2,12 +2,35 @@ import pandas as pd
 
 
 
-df = pd.read_excel("Z:\Group project\Contact emails\CONTACT EMAILS.xlsx")
+#Windows df = pd.read_excel("Z:\Group project\Contact emails\CONTACT EMAILS.xlsx")
 
-print(df)
+#Mac
+
+df = pd.read_excel("/Users/Jonny/Desktop/University-chatbot/Contact emails/CONTACT EMAILS.xlsx")
 
 
 
+
+
+department = df['Department'].head
+
+
+
+# 3. Update only the first 15 rows of the 'Name' column
+# .iloc[rows, columns] -> [0:15] means rows 0 to 14
+df.iloc[0:15, df.columns.get_loc('Emails')] = df.iloc[0:15, df.columns.get_loc('Name')] + '@huddersfield.com'
+
+
+
+
+
+
+print("Updated the first 15 names successfully!")
+
+
+
+# Save it back to Excel so the changes stick
+df.to_excel("/Users/Jonny/Desktop/University-chatbot/Contact emails/CONTACT EMAILS.xlsx", index=False)
 
 
 
